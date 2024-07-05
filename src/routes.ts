577cd56af6110a5839ffe02d2bd8559300bd0655
bruno.blanquez <b.blanquez@yahoo.com.br>
2024-07-05 15:52:44 -0300

    First commit

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+
+const router = Router();
+
+router.post('/users', new CreateUserController().handle)
+router.post('/login', new AuthUserController().handle)
+
+export {router};
